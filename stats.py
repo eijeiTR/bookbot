3493c2c48bba3486b07c08050c4921cfeb873c3b
eijeiTR <010santosa@gmail.com>
2025-10-29 14:17:37 -0400

    B: stats problem solved. needs formatting.

--- stats.py
+++ stats.py
@@ -1,3 +1,7 @@
+# takes the contents of a file (path) as input.
+# separates each word delimited by spaces.
+# iterates through the info stored in the words variable.
+# returns the total count of words.
 def count_words(file_contents):
     text = file_contents
     words = text.split()
@@ -7,16 +11,18 @@ def count_words(file_contents):
         count += 1
     return count
 
+# takes the contents of a file (path) as input.
+# sets all the text to lowercase to keep uniqueness.
+# iterates through the info stored in the text variable.
+# increments the count if char matches the dict key.
+# initializes a unique dictionary key->value pair ({char:count}) if not present.
 def count_char(file_contents):
     text = file_contents.lower()
-    unique_char = set()
     char_count_dict = {}
 
     for char in text:
-        unique_char.add(char)
-    for char in unique_char:
-        if char in unique_char:
-            count = char_count_dict[char]
-            #print(f"CHAR: {char}, COUNT: {count}")
-        return char_count_dict
-    
+        if char in char_count_dict:
+            char_count_dict[char] += 1
+        else:
+            char_count_dict[char] = 1
+    return char_count_dict
