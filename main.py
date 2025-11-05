a5a0a378c6a41d5ef05f0f51e5fd8ac3605926cc
eijeiTR <010santosa@gmail.com>
2025-11-05 14:08:57 -0500

    D: added more books and completed the bookbot! :)

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import (
         count_words,
         count_char,
@@ -5,13 +6,17 @@ from stats import (
 )
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book_path = sys.argv[1]
     
-    word_count = count_words(get_book_text(book_path))
-    char_count = count_char(get_book_text(book_path))
-    char_sorted_list = chars_dict_to_sorted_list(char_count)
+        word_count = count_words(get_book_text(book_path))
+        char_count = count_char(get_book_text(book_path))
+        char_sorted_list = chars_dict_to_sorted_list(char_count)
 
-    print_report(book_path, word_count, char_sorted_list)
+        print_report(book_path, word_count, char_sorted_list)
 
 def get_book_text(file_path):
     with open(file_path) as f: # man with for a description of what the function does
